2ba12665fd1d7d8a975eeb04e2640610f4e92fac
Jose Tovar <jtovar26455462@gmail.com>
2025-11-04 16:54:50 -0400

    fix: adapt to vercel configs

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function createApp() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  // Swagger configuration
   const config = new DocumentBuilder()
     .setTitle('Choppi API')
     .setDescription('Choppi e-commerce API documentation')
@@ -18,9 +17,26 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 4000);
-  console.log(`Application is running on: http://localhost:${process.env.PORT ?? 4000}`);
-  console.log(`Swagger documentation: http://localhost:${process.env.PORT ?? 4000}/api`);
+  await app.init();
+  return app;
 }
 
-bootstrap();
+export default async (req: any, res: any) => {
+  const app = await createApp();
+  const server = app.getHttpAdapter().getInstance();
+
+  return server(req, res);
+};
+
+async function runLocal() {
+  const app = await createApp();
+  const port = process.env.PORT || 4000;
+
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Swagger documentation: http://localhost:${port}/api`);
+}
+
+if (process.env.VERCEL_ENV === undefined) {
+  runLocal();
+}
